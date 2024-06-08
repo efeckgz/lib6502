87df9d0bd794d2b443fec82ffbd84c3248babc79
efeckgz <efeckgz@gmail.com>
2024-06-08 12:09:38 +0300

    Moved test module into cpu. Removed useless pub from register fields. derived debug for cpu.

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -14,11 +14,12 @@ pub enum FlagBitPos {
 
 type InstructionExecuter<M> = fn(&mut CPU<M>, u16);
 
+#[derive(Debug)]
 pub struct CPU<M: Memory> {
-    pub a: u8,
-    pub x: u8,
-    pub y: u8,
-    pub pc: u16,
+    a: u8,
+    x: u8,
+    y: u8,
+    pc: u16,
     sp: u8,
     flag: u8,
 
@@ -159,7 +160,7 @@ impl<M: Memory + 'static> CPU<M> {
 
         self.set_flag(FlagBitPos::Carry, did_overflow);
         self.set_flag(FlagBitPos::Zero, self.a == 0);
-        // I have no idea how this works
+        // The sign of both inputs is different than the sign of the output.
         self.set_flag(
             FlagBitPos::Overflow,
             ((self.a ^ result) & (to_add ^ result) & 0x80) != 0,
@@ -202,5 +203,159 @@ impl<M: Memory + 'static> CPU<M> {
         // Set the status flags
         self.set_flag(FlagBitPos::Negative, (val as i8) < 0);
         self.set_flag(FlagBitPos::Zero, val == 0);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    // use self::cpu::CPU;
+    use super::*;
+    // use crate::cpu::FlagBitPos;
+    // use memory::Memory;
+
+    #[derive(Debug)]
+    struct Mem {
+        pub bytes: [u8; 4096],
+    }
+
+    impl Memory for Mem {
+        fn read_byte(&self, addr: u16) -> u8 {
+            self.bytes[addr as usize]
+        }
+
+        fn write_byte(&mut self, addr: u16, data: u8) {
+            self.bytes[addr as usize] = data;
+        }
+
+        fn reset(&mut self) {
+            self.bytes = [0; 4096];
+        }
+    }
+
+    impl Mem {
+        fn new() -> Mem {
+            Mem { bytes: [0; 4096] }
+        }
+    }
+
+    #[test]
+    fn adc_works() {
+        let memory = Mem::new();
+        let mut cpu = CPU::new(memory);
+
+        let tests = vec![
+            (0x69_u8, 0xFF_u8, 0x18_u8),
+            (0x69_u8, 0x1A_u8, 0xBD_u8),
+            (0x69_u8, 0xFF_u8, 0x01_u8),
+        ];
+
+        let mut test_index = 1;
+        for (opcode, add_1, add_2) in tests {
+            cpu.reset();
+            let (result, did_overflow) = add_1.overflowing_add(add_2);
+            // let program = vec![opcode, add_1, opcode, add_2];
+            let program: [u8; 4] = [opcode, add_1, opcode, add_2];
+            cpu.load_program(&program);
+            cpu.run_for(2);
+
+            assert_eq!(cpu.a, result);
+            assert!(cpu.flag_raised(FlagBitPos::Carry) == did_overflow);
+            println!("Test {test_index} passed.");
+            test_index += 1;
+        }
+    }
+
+    #[test]
+    fn abs_adc_works() {
+        let memory = Mem::new();
+        let mut cpu = CPU::new(memory);
+
+        let lo = 0x10_u8;
+        let hi = 0x06_u8;
+        let addr: u16 = ((hi as u16) << 8) | (lo as u16); // should be 0x610
+        println!("addr: {addr:#04x}");
+
+        let mut program: [u8; 2048] = [0; 2048]; // 2k bytes of 0.
+        program[0] = 0x6D_u8; // Absolute addressing adc instruction
+        program[1] = lo;
+        program[2] = hi;
+        program[0x10] = 0x19_u8; // memory address 0x610 will have the value 0x19
+
+        cpu.load_program(&program);
+
+        cpu.run_for(1);
+        assert_eq!(cpu.a, 0x19);
+    }
+
+    #[test]
+    fn and_works() {
+        let memory = Mem::new();
+        let mut cpu = CPU::new(memory);
+
+        // A test value to load to the a register.
+        // lda with test_a, then and a with test_val
+        let test_a: u8 = 0x15;
+        let test_val: u8 = 0x01; // we will and accumulator with this value.
+
+        let program: [u8; 4] = [0xA9, test_a, 0x29, test_val];
+        cpu.load_program(&program);
+
+        cpu.run_for(2);
+        assert_eq!(cpu.a, test_a & test_val);
+    }
+
+    #[test]
+    fn lda_works() {
+        let memory = Mem::new();
+        let mut cpu = CPU::new(memory);
+
+        let mut program = [0; 2048];
+        program[0] = 0xA9_u8;
+        program[1] = 0x00_u8;
+
+        // program[0] = 0xA5_u8; // at 0x600 - zero page addressing
+        // program[1] = 0xFA_u8; // at 0x601
+        // program[0x00FA - 0x600] = 0x15;
+
+        cpu.load_program(&program);
+        cpu.run_for(1);
+
+        assert_eq!(cpu.a, 0x00);
+        assert!(!cpu.flag_raised(FlagBitPos::Negative));
+        assert!(cpu.flag_raised(FlagBitPos::Zero));
+    }
+
+    #[test]
+    fn ldx_works() {
+        let memory = Mem::new();
+        let mut cpu = CPU::new(memory);
+
+        let mut program = [0; 2048];
+        program[0] = 0xA2_u8;
+        program[1] = 0x24_u8;
+
+        cpu.load_program(&program);
+        cpu.run_for(1);
+
+        assert_eq!(cpu.x, 0x24);
+        assert!(!cpu.flag_raised(FlagBitPos::Negative));
+        assert!(!cpu.flag_raised(FlagBitPos::Zero));
+    }
+
+    #[test]
+    fn ldy_works() {
+        let memory = Mem::new();
+        let mut cpu = CPU::new(memory);
+
+        let mut program = [0; 2048];
+        program[0] = 0xA0_u8;
+        program[1] = 0x24_u8;
+
+        cpu.load_program(&program);
+        cpu.run_for(1);
+
+        assert_eq!(cpu.y, 0x24);
+        assert!(!cpu.flag_raised(FlagBitPos::Negative));
+        assert!(!cpu.flag_raised(FlagBitPos::Zero));
     }
 }
