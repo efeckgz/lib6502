fb00748d9f817c9d83f591fbb5289699f2737099
efeckgz <efeckgz@gmail.com>
2025-05-09 15:35:10 +0300

    Started work on json test framework.

--- tests/A9.rs
+++ tests/A9.rs
@@ -1,23 +1,74 @@
+use std::fmt::format;
+use std::{fs, io};
+
 use lib6502::Memory;
 use lib6502::bus::Bus;
 use lib6502::cpu::{Cpu, RegisterState};
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+const TESTS_DIR: &str = "./65x02/6502/v1";
+
+#[derive(Serialize, Deserialize, Debug)]
 struct Test {
+    name: String,
+
+    #[serde(rename(deserialize = "initial"))]
     initial_state: State,
+
+    #[serde(rename(deserialize = "final"))]
     final_state: State,
-    cycles: (u16, u8, bool), // address, value, read/write
+
+    cycles: Vec<(u16, u8, String)>, // address, value, read/write
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 struct State {
-    registers: RegisterState,
+    pc: u16,
+    s: u8,
+    a: u8,
+    x: u8,
+    y: u8,
+    p: u8,
     ram: Vec<(u16, u8)>,
+}
+
+impl State {
+    pub fn from_registers((pc, s, a, x, y, p): RegisterState) -> Self {
+        Self {
+            pc,
+            s,
+            a,
+            x,
+            y,
+            p,
+            ram: vec![],
+        }
+    }
 }
 
 #[test]
 fn it_works() {
+    let test = load_test("A9");
+    println!("{:?}", test);
     assert_eq!(42, 42);
+}
+
+fn get_entries() -> Vec<std::path::PathBuf> {
+    let mut entries = fs::read_dir(TESTS_DIR)
+        .unwrap()
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()
+        .unwrap();
+
+    entries.sort();
+
+    entries
+}
+
+fn load_test(name: &str) -> Vec<Test> {
+    let test_name = TESTS_DIR.to_owned() + &format!("/{}.json", name);
+    println!("Test name: {}", test_name);
+    let bytes = fs::read(test_name).unwrap();
+    serde_json::from_slice(&bytes).unwrap()
 }
